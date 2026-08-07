23f0c8169e4b3cc1186d999ac4f4a2903db3191b
Chris Burke <cburke@CJB-XPS.localdomain>
2026-08-06 21:14:32 -0700

    Added experimentation macros and comments on results of experimentation to date.

--- linuxdoom-1.10/tables.h
+++ linuxdoom-1.10/tables.h
@@ -47,7 +47,46 @@
 
 #include "m_fixed.h"
 
-#define FIXED_MASK_CJB		0xFFFFFFFFL
+// The tan and arctan tables use 32-bit fixed
+// point numbers. The sine table uses 17-bit
+// fixed point numbers (-65525 to +65535).
+//
+// These masks are used to experiment with
+// lower numbers of significant bits in the
+// table values.
+
+// To try less-precise tan and arctan.
+// Use 0xFFFFFFFF for full precision.
+//
+// Based on experimentation, the lowest usable
+// tan precision is about 19 bits of TAN_MASK.
+//
+// Examining the values in finetangent shows
+// that its values use only about 29 bits of
+// the available 32 bits. This means 19 bits
+// of TAN_MASK are equivalent to 16 significant
+// bits in each finetangent value, suggesting
+// we could use a 16-bit table for finetangent.
+//
+// The values in tantoangle (arctangent) also
+// use only about 29 bits of the available
+// 32 bits, suggesting we could use a 16-bit
+// table for tantoangle.
+
+//#define TAN_MASK_CJB	0xFFFFE000
+#define TAN_MASK_CJB	0xFFFFFFFF
+
+// To try less-precise sin and cos.
+// Use 0xFFFFFFFF for full precision but
+// the top 15 bits are sign extension.
+//
+// Based on experimentation, the lowest usable
+// sin precision is about 6 bits. For precisions
+// of 8 bits and below, a table with 256 entries
+// per 2pi is sufficient for low-end performance.
+
+//#define SIN_MASK_CJB	0xFFFFF800
+#define SIN_MASK_CJB	0xFFFFFFFF
 
 #define FINEANGLES		8192
 #define FINEMASK		(FINEANGLES-1)
@@ -58,18 +97,18 @@
 
 // Effective size is 10240.
 extern  fixed_t		finesine[5*FINEANGLES/4];
-#define FINESINE(theta) (INT32)(finesine[(theta)] & FIXED_MASK_CJB)
+#define FINESINE(theta) (INT32)(finesine[(theta)] & SIN_MASK_CJB)
 //#define FINESINE(theta) finesine[(theta)]
 
 // Re-use data, is just PI/2 pahse shift.
 extern  fixed_t*	finecosine;
-#define FINECOSINE(theta) (INT32)(finecosine[(theta)] & FIXED_MASK_CJB)
+#define FINECOSINE(theta) (INT32)(finecosine[(theta)] & SIN_MASK_CJB)
 //#define FINECOSINE(theta) finecosine[(theta)]
 
 
 // Effective size is 4096.
 extern fixed_t		finetangent[FINEANGLES/2];
-#define FINETANGENT(theta) (INT32)(finetangent[(theta)] & FIXED_MASK_CJB)
+#define FINETANGENT(theta) (INT32)(finetangent[(theta)] & TAN_MASK_CJB)
 //#define FINETANGENT(theta) finetangent[(theta)]
 
 // Binary Angle Measument, BAM.
@@ -90,7 +129,7 @@ typedef UINT32 angle_t;
 // The +1 size is to handle the case when x==y
 //  without additional checking.
 extern angle_t		tantoangle[SLOPERANGE+1];
-#define TANTOANGLE(m) (INT32)(tantoangle[(m)] & FIXED_MASK_CJB)
+#define TANTOANGLE(m) (INT32)(tantoangle[(m)] & TAN_MASK_CJB)
 //#define TANTOANGLE(m) tantoangle[(m)]
 
 
